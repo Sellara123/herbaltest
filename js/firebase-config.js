ff2037822ea8eeb1daaf2b811e4138925f23d846
Sellara123 <oranghitam73@gmail.com>
2026-04-23 18:30:04 +0700

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+// Ganti dengan konfigurasi Firebase Anda sendiri
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Referensi ke node produk
+const productsRef = database.ref('products');
